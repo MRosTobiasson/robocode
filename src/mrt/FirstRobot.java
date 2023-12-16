612c5bf244ab35267dfaef35d7647ca045875a27
MRosTobiasson <117571587+MRosTobiasson@users.noreply.github.com>
2023-12-16 23:05:27 +0100

    first commit

--- src/mrt/FirstRobot.java
+++ src/mrt/FirstRobot.java
@@ -1,7 +1,24 @@
 package mrt;
 
 import robocode.Robot;
+import robocode.ScannedRobotEvent;
 
 public class FirstRobot extends Robot {
+
+    public void run() {
+
+        while (true) {
+            ahead(100);
+            turnGunRight(360);
+            back(100);
+            turnGunRight(360);
+        }
+    }
+
+    public void onScannedRobot(ScannedRobotEvent e) {
+
+        fire(1);
+
+    }
 
 }
