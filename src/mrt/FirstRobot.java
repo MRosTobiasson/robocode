ca7ff6ee66c1c7df4aab77b5e12ffbd31cbaa9f5
MRosTobiasson <117571587+MRosTobiasson@users.noreply.github.com>
2023-12-19 18:39:49 +0100

    FirstRobot. Added simple scanning

--- src/mrt/FirstRobot.java
+++ src/mrt/FirstRobot.java
@@ -1,24 +1,40 @@
 package mrt;
 
-import robocode.Robot;
+import robocode.AdvancedRobot;
+import robocode.RobotDeathEvent;
 import robocode.ScannedRobotEvent;
 
-public class FirstRobot extends Robot {
+public class FirstRobot extends AdvancedRobot {
+    private byte scanDirection = 1;
+    private EnemyBot enemy = new EnemyBot();
 
     public void run() {
-
+        setAdjustRadarForRobotTurn(true);
+        enemy.reset();
         while (true) {
-            ahead(100);
-            turnGunRight(360);
-            back(100);
-            turnGunRight(360);
+            setTurnRadarRight(360);
+            execute();
         }
     }
 
     public void onScannedRobot(ScannedRobotEvent e) {
+        setTurnRadarRight(getHeading() - getRadarHeading() + e.getBearing());
+        if (enemy.none() || e.getName().equals(enemy.getName())) {
+            enemy.update(e);
+        }
+        scanDirection *= -1;
+        setTurnRadarRight(360 * scanDirection);
 
+        if (enemy.none() ||
+                e.getDistance() < enemy.getDistance() || e.getName().equals(enemy.getName())) {
+            enemy.update(e);
+        }
         fire(1);
-
     }
 
+    public void onRobotDeath(RobotDeathEvent e) {
+        if (e.getName().equals(enemy.getName())) {
+            enemy.reset();
+        }
+    }
 }
